2bed9581754a090daafbc15507b88a3dacd42907
Victoria Silva <Victoria Silva@DESKTOP-TP3RSDB>
2022-07-06 12:41:33 -0300

    Adicionar arquivos de projeto.

---
+++ exercico 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercico_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, tb, media, conceito;
+            string aluno;
+
+            Console.WriteLine("Digite o nome completo do aluno:");
+            aluno = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota:");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota:");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do trabalho;");
+            tb = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(aluno);
+
+            media = n1 + n2 + tb;
+            conceito = media / 3;
+
+            if (conceito >= 8.5)
+            {
+                Console.WriteLine("Nota A");
+
+            }
+            else if (conceito >= 7.0)
+            {
+                Console.WriteLine("Nota B");
+
+            }
+            else if (conceito >=6.0)
+            {
+                Console.WriteLine("Nota C");
+            }
+            else if (conceito >=0.1)
+            {
+                Console.WriteLine("Nota D");
+            }
+            else if (conceito <=0)
+            {
+                Console.WriteLine("Nota E");
+            }
+            Console.WriteLine(conceito);
+
+            Console.ReadKey();
+        }
+    }
+}
